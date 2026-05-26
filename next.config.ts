f4b683e0d63d648fb3583b166707d42c4ae19d21
stopcracking <fnoworres@gmail.com>
2026-05-26 23:57:27 +0300

    Remove static export to enable API routes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import { NextConfig } from "next";
 import { PHASE_PRODUCTION_BUILD } from "next/constants";
 
 const nextConfig = (phase: string): NextConfig => ({
-  output: phase === PHASE_PRODUCTION_BUILD ? "export" : undefined,
   trailingSlash: false,
   reactStrictMode: true,
   reactCompiler: true,
@@ -31,7 +30,6 @@ const nextConfig = (phase: string): NextConfig => ({
 });
 
 export default withSentryConfig(nextConfig, {
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
   org: process.env.SENTRY_ORG,
   project: "javascript-nextjs",
   widenClientFileUpload: true,
